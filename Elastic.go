28895f2516df288c7a3b12a022c44c0bf25c27e5
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-11-06 17:07:35 +0100

    Finished Go code

--- Elastic.go
+++ Elastic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -19,9 +20,7 @@ func Exit() {
 }
 
 func Get(reader *bufio.Scanner) {
-	fmt.Print("Enter spacecraft ID: ")
-	reader.Scan()
-	id := reader.Text()
+	id := ReadText(reader, "Enter spacecraft ID")
 	request := esapi.GetRequest{Index: "stsc", DocumentID: id}
 	response, _ := request.Do(context.Background(), es)
 	var results map[string]interface{}
@@ -29,14 +28,51 @@ func Get(reader *bufio.Scanner) {
 	Print(results["_source"].(map[string]interface{}))
 }
 
+func Search(reader *bufio.Scanner, querytype string) {
+	key := ReadText(reader, "Enter key")
+	value := ReadText(reader, "Enter value")
+	var buffer bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			querytype: map[string]interface{}{
+				key: value,
+			},
+		},
+	}
+	json.NewEncoder(&buffer).Encode(query)
+	response, _ := es.Search(es.Search.WithIndex("stsc"), es.Search.WithBody(&buffer))
+	var result map[string]interface{}
+	json.NewDecoder(response.Body).Decode(&result)
+	for _, hit := range result["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		craft := hit.(map[string]interface{})["_source"].(map[string]interface{})
+		Print(craft)
+	}
+}
+
+func ReadText(reader *bufio.Scanner, prompt string) string {
+	fmt.Print(prompt + ": ")
+	reader.Scan()
+	return reader.Text()
+}
+
 func Print(spacecraft map[string]interface{}) {
 	name := spacecraft["name"]
 	status := ""
 	if spacecraft["status"] != nil {
 
-		status = "- status: " + spacecraft["status"].(string)
+		status = "- " + spacecraft["status"].(string)
 	}
-	fmt.Println(name, status)
+	registry := ""
+	if spacecraft["registry"] != nil {
+
+		registry = "- " + spacecraft["registry"].(string)
+	}
+	class := ""
+	if spacecraft["spacecraftClass"] != nil {
+
+		class = "- " + spacecraft["spacecraftClass"].(map[string]interface{})["name"].(string)
+	}
+	fmt.Println(name, registry, class, status)
 }
 
 func main() {
@@ -45,15 +81,19 @@ func main() {
 		fmt.Println("0) Exit")
 		fmt.Println("1) Load spacecraft")
 		fmt.Println("2) Get spacecraft")
-		fmt.Print("Enter option: ")
-		reader.Scan()
-		option := reader.Text()
+		fmt.Println("3) Search spacecraft by key and value")
+		fmt.Println("4) Search spacecraft by key and prefix")
+		option := ReadText(reader, "Enter option")
 		if option == "0" {
 			Exit()
 		} else if option == "1" {
 			LoadData()
 		} else if option == "2" {
 			Get(reader)
+		} else if option == "3" {
+			Search(reader, "match")
+		} else if option == "4" {
+			Search(reader, "prefix")
 		} else {
 			fmt.Println("Invalid option")
 		}
